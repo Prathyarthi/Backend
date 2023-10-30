559e127f8888c250048695ad25ba383abb9dcfd6
Prathyarthi <prathyarti@gmail.com>
2023-10-30 14:11:20 +0530

    Created ecommerce models to learn data modeling

---
+++ project2/models/ecommerce/order.model.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose'
+
+const orderItemsSchema = new mongoose.Schema({
+    productId: {
+        type: mongoose.Schem.Types.ObjectId,
+        ref: 'Product'
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+})
+
+const orderSchema = new mongoose.Schema({
+    orderPrice: {
+        type: Number,
+        required: true
+    },
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    orderItems: {
+        type: [orderItemsSchema]
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["Pending", "Cancelled", "Delivered"],   //enum i.e enumerations are nothing but the options(We can choose only from the given options)
+        default: "Pending"
+    }
+}, { timestamps: true })
+
+export const Order = mongoose.model("Category", orderSchema);
